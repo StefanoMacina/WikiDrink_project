0b0201e2ecbe9e2efd8d40ac6dffb83fbd599e33
Stefano <maclion@live.com>
2023-08-12 11:16:20 +0200

    add : form backend and navigation

--- WikiDrink/src/screen/Contattaci.jsx
+++ WikiDrink/src/screen/Contattaci.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Hero from '../components/Hero'
 import background from '../assets/image/Contact-hero.png'
+import axios from 'axios'
+import { useNavigate} from'react-router-dom'
+
 
 const Contattaci = () => {
+  const inputFocus = useRef()
+  const navigate = useNavigate()
+
+
+  const [data, setData] = useState({
+    name : '',
+    lastname : '',
+    email : '',
+    tel : '',
+    recipe : ''
+
+  })
+
+  const handleChange = (e) => {
+    const {name,value} = e.target
+    setData((prevData) => ({...prevData , [name] : value}))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    postData()
+    console.log(data)
+  }
+
+  const postData = async() =>{
+    try {
+      const res = await axios.post('http://localhost:3000/find' , data, {
+        headers : {
+          "Content-Type": "application/json",
+        }
+      })
+      if(res.status === 200){
+        setData({
+          name : '',
+          lastname : '',
+          email : '',
+          tel : '',
+          recipe : ''
+        })
+        setTimeout(() => {
+          navigate("/")
+        }, 1500);
+      }
+      
+    } catch (error) {
+      console.log(error.name);
+    }
+  
+  }
+
+  useEffect(() => {
+    inputFocus.current.focus()
+  },[])
+
+
+
   return (
    <Hero disabledOverlay img={background}>
     <section className='contact-us-container'>
@@ -10,30 +69,33 @@ const Contattaci = () => {
         <h2>Vorresti aggiungere un cocktail?</h2>
         <p>Il nostro team è sempre disponibile per valutare possibili nuove ricette ed ad aggiungerle alla nostro database</p>
       </div>
-      <form action="" className='contact-us-form'>
+      <form action="/find" method='POST' className='contact-us-form' onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Nome</label>
-          <input type="text" name='name' placeholder='name' />
+          <label htmlFor="name">Name</label>
+          <input type="text" name='name' value={data.name} onChange={handleChange} placeholder='name' ref={inputFocus} />
           <hr />
         </div>
         <div>
           <label htmlFor="lastname">lastname</label>
-          <input type="text"  name='lastname' placeholder='lastname' />
+          <input type="text"  name='lastname' value={data.lastname} onChange={handleChange} placeholder='lastname' />
           <hr />
         </div>
         <div>
           <label htmlFor="email">email</label>
-          <input type="email" name='email' placeholder='daniele@esempio.com' />
+          <input type="email" name='email' value={data.email} onChange={handleChange} placeholder='daniele@esempio.com' />
           <hr />
         </div>
         <div>
           <label htmlFor="phone">telefono</label>
-          <input type="tel" name='tel' placeholder='555 555-55' />
+          <input type="tel" name='tel' value={data.tel} onChange={handleChange} placeholder='555 555-55' />
           <hr />
         </div>
         <div>
           <label htmlFor="recipe">recipe</label>
-          <textarea id='recipe' name='recipe' placeholder='descrivi la tua ricetta'  />
+          <textarea id='recipe' name='recipe' value={data.recipe} onChange={handleChange} placeholder='descrivi la tua ricetta'  />
+        </div>
+        <div className='form-send-btn'>
+          <button>SEND RECIPE</button>
         </div>
       </form>
     </section>
--- WikiDrink/src/screen/Home.jsx
+++ WikiDrink/src/screen/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
+import homeImg from "../assets/image/home-hero.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div></div>
-  )
-}
+  
+      <Hero img={homeImg} >
+        <div className="home-container">
+          <div className="home-first-container">
+            <div className="home-text-container">
+              <h1>Wiki drink</h1>
+              <h3>tutti i cocktail del mondo a portata di click</h3>
+              <p>
+                Wiki Drink è un database internazionale che mette a tua
+                disposizione, in maniera <span>Gratuita</span>, le ricette dei più importanti e
+                diffusi cocktail al mondo.
+              </p>
+            <button className="home-cta-btn"><Link to='/about'>scopri di più</Link></button>
+            </div>
+          </div>
+            <div className="image">
+              sdsds
+            </div>
+          </div>
+      </Hero>
+   
+  );
+};
 
-export default Home
+export default Home;
